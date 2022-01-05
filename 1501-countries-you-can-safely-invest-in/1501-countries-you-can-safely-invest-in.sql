323b203e8fd749e6b5ebccc9c1b34dbbfd935c80
Delin "Samuel" Zhu <93402973+dlszhu@users.noreply.github.com>
2022-01-05 11:59:42 -0800

    Time: 1011 ms (53.85%), Space: 0B (100.00%) - LeetHub

--- 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -20,3 +20,15 @@ GROUP BY
     #group by rows with same country and calculate the duration
 HAVING 
     AVG(duration) > (SELECT AVG(duration) FROM Calls)
+    
+    #alternatively use window function
+    #select distinct country 
+    #    from(
+    #        select c.name country, ca.duration
+    #            ,avg(ca.duration) over(partition by c.name) avg_duration
+    #            ,avg(ca.duration) over() avg_total
+    #        from calls ca 
+    #        left join person p on (ca.caller_id = p.id) or (ca.callee_id = p.id)
+    #        inner join country c on substring(p.phone_number,1,3) = c.country_code
+    #    ) a
+    #    where avg_duration > avg_total
