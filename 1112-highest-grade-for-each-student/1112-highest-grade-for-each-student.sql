893d3845d24933c719463c62409970912702944b
Delin "Samuel" Zhu <93402973+dlszhu@users.noreply.github.com>
2022-01-12 10:19:11 -0800

    Time: 766 ms (13.61%), Space: 0B (100.00%) - LeetHub

--- 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
+++ 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
@@ -1,30 +1,16 @@
-# Write your MySQL query statement below
+#much easier solution using Row number window function and order by grade and course id
 
-#to get only the max grade from table
-#used a cte table to have the max value first and use join to match
+WITH P AS (SELECT 
+                *, 
+                ROW_NUMBER() OVER(PARTITION BY student_id 
+                                  ORDER BY grade DESC, course_id) AS grader
+           FROM Enrollments)
+           
+SELECT 
+    student_id, 
+    course_id, 
+    grade
+FROM P
+WHERE grader = 1;
 
-with cte as (select
-    student_id id,
-    max(grade) g
-from
-    Enrollments
-group by
-    student_id)
-
-#the hard part here is to select only the first record when there is a tie in max grade
-#MySQL has no DISTINCT ON like postgresql
-#but group by the columns that do not need to return first record do the same job
-#so here we group by student_id and grade
-
-select 
-    e.student_id,
-    min(e.course_id) as course_id,
-    e.grade
-from
-    Enrollments e
-    join cte on e.student_id = cte.id and e.grade=cte.g
-group by
-    e.student_id, e.grade
-order by
-    e.student_id, e.grade desc
     
