7bdcf66ba3793f8e170a85742814579369c35181
Delin "Samuel" Zhu <93402973+dlszhu@users.noreply.github.com>
2022-01-13 11:12:46 -0800

    Time: 720 ms (84.10%), Space: 0B (100.00%) - LeetHub

--- 1126-active-businesses/1126-active-businesses.sql
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -1,18 +1,16 @@
 # Write your MySQL query statement below
 
-#a type of question seen quite often:
-#compare the value of a specific row with the whole category's avg/sum etc.
-
-select business_id                                      # Finally, select 'business_id'
-from
-(select event_type, avg(occurences) as ave_occurences   # First, take the average of 'occurences' group by 'event_type'
- from events as e1
- group by event_type
-) as temp1
-join events as e2 on temp1.event_type = e2.event_type   # Second, join Events table on 'event_type'
-where e2.occurences > temp1.ave_occurences              # Third, the 'occurences' should be greater than the average of 'occurences'
-group by business_id
-having count(distinct temp1.event_type) > 1             # (More than one event type with 'occurences' greater than 1)
+WITH r2 AS(
+    SELECT *, 
+    CASE 
+        WHEN occurences > AVG(occurences) OVER (PARTITION BY event_type) 
+        THEN 1 ELSE 0 END AS chosen
+    FROM Events)
+    
+SELECT business_id
+FROM r2
+GROUP BY business_id
+HAVING SUM(chosen) >1;
     
 
     
