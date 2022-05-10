b2e4a0e856da5bf2337c0ce0506f33858827baa1
Delin "Samuel" Zhu <93402973+dlszhu@users.noreply.github.com>
2022-05-10 11:40:22 -0700

    Time: 1079 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -3,14 +3,10 @@
 select
     sell_date,
     count(distinct product) as num_sold,
-    #count distinct product as duplicate exists
-    GROUP_CONCAT(distinct product ORDER BY product) as products
-    #GROUP_CONCAT = STRING_AGG in PostGreSQL
-    #ORDER BY in function to order
-    #STRING_AGG() WITHIN GROUP (ORDER BY)
-from 
+    group_concat(distinct product) as products
+from
     Activities
-group by 
-    sell_date
-order by 
-    sell_date
+group by sell_date
+
+#string_agg in postgresql and group_concat in mysql
+#to list all string values after groupby
